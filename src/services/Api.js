ec21aa02c8b276ee7caf6cfd4b6ead5643582a69
Justin Langlinais <justinclanglinais@gmail.com>
2020-09-16 16:04:18 -0700

    feat(Api): bbuild fetchCards and export

---
+++ src/services/Api.js
@@ -0,0 +1,16 @@
+const API_ROOT = `http://localhost:3000`
+
+const headers = {
+    'Content-Type' : 'application/json',
+    Accepts : 'application/json',
+}
+
+const fetchCards = () => {
+    return fetch(`${API_ROOT}/cards`).then(r=>r.json())
+}
+
+export const Api = {
+    cards: {
+        fetchCards
+    }
+]
